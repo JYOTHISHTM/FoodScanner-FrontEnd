b06cad8d56ec12a57af9e73b69ae20bdd2760b63
Jyothish T M <jothishtm2002@gmail.com>
2026-03-27 13:54:28 +0530

    feat: integrate Google OAuth login UI and backend connection

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+
 
 createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId='706114962131-mv4s355lqpq4ct7k4rf25md34q2jqosq.apps.googleusercontent.com'>
   <StrictMode>
     <App />
   </StrictMode>,
+  </GoogleOAuthProvider>
 )
