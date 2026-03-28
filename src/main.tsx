c0be9bb53d08b620579b75c91b53fed1d1d67f66
Jyothish T M <jothishtm2002@gmail.com>
2026-03-28 10:59:12 +0530

    feat(auth): wrap app with AuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { AuthProvider } from "./context/AuthContext";
 
 
 
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId='706114962131-mv4s355lqpq4ct7k4rf25md34q2jqosq.apps.googleusercontent.com'>
   <StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </StrictMode>,
   </GoogleOAuthProvider>
 )
