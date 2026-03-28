7301fd8615f175173410809347ae2a8cb2eb6037
Jyothish T M <jothishtm2002@gmail.com>
2026-03-28 12:09:01 +0530

    feat(profile): add profile page to display user name and email

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,18 @@
+
+
+import { useAuth } from "../hooks/useAuth";
+
 const Profile = () => {
-  return <h1>User Profile</h1>;
+  const { user } = useAuth();
+
+  return (
+    <div>
+      <h1>Profile</h1>
+
+      <p>Name: {user?.name}</p>
+      <p>Email: {user?.email}</p>
+    </div>
+  );
 };
 
 export default Profile;
