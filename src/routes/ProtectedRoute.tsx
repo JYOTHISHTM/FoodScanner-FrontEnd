168fb4b4e622941804c6b08356c0ff6b36c5b3f9
Jyothish T M <jothishtm2002@gmail.com>
2026-03-28 10:57:05 +0530

    feat(auth): add protected and public route components

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+
+
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+
+const ProtectedRoute = ({ children }: {children: React.ReactNode }) => {
+  const { isLoggedIn } = useAuth();
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
