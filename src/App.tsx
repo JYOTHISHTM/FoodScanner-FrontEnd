30eabe98e44fded2529ac5150cefc02c9b853f70
Jyothish T M <jothishtm2002@gmail.com>
2026-03-31 19:15:03 +0530

    feat: update app layout and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,26 +10,39 @@ import ProductInfo from "./pages/ProductInfo";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-
 import ProtectedRoute from "./routes/ProtectedRoute";
 import PublicRoute from "./routes/PublicRoute";
 
+function Layout() {
+  const location = useLocation();
+
+  const hideLayout = location.pathname === "/login";
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      
+      {!hideLayout && <Navbar />}
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route path="/history" element={<ProtectedRoute><History /></ProtectedRoute>} />
+          <Route path="/product/:id" element={<ProtectedRoute><ProductInfo /></ProtectedRoute>} />
+        </Routes>
+      </main>
+
+      {!hideLayout && <Footer />}
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-        <Route path="/login" element={ <PublicRoute><Login /></PublicRoute>} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-        <Route path="/history" element={<ProtectedRoute><History /></ProtectedRoute>} />
-        <Route path="/product/:id" element={<ProtectedRoute><ProductInfo /></ProtectedRoute>} />
-      </Routes>
-
-      <Footer />
+      <Layout />
     </Router>
   );
 }
