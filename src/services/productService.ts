0efb15e3e43b394a2df152ff905662b5253c278a
Jyothish T M <jothishtm2002@gmail.com>
2026-04-06 15:58:02 +0530

    feat: update product service to support favorites

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,7 +1,11 @@
 // services/productService.ts
 import axios from "axios";
 
-export const fetchProduct = async (barcode: string) => {
-  const res = await axios.get(`http://localhost:4000/api/product/${barcode}`);
+export const fetchProduct = async (productId: string,userId: string) => {
+  const res = await axios.get(`http://localhost:4000/api/product/${productId}`,
+    {
+      params: { userId } // ✅ send userId
+    }
+  );
   return res.data;
 };
