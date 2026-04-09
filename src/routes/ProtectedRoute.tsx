0962a1e7653ff21d54ff8e4908c7636b95e3115c
Jyothish T M <jothishtm2002@gmail.com>
2026-04-09 21:35:41 +0530

    refactor(routes): update ProtectedRoute logic

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,28 +1,5 @@
 
 
-
-// import { Navigate, Outlet } from "react-router-dom";
-// import { useAuth } from "../hooks/useAuth";
-// import { useAdminAuth } from "../context/AdminAuthContext";
-
-// const ProtectedRoute = () => {
-//   const { isLoggedIn } = useAuth();
-//   const { isAuthenticated } = useAdminAuth();
-
-//   if (!isLoggedIn) {
-//     return <Navigate to="/login" />;
-//   }
-
-//   if (!isAuthenticated) {
-//     return <Navigate to="/admin/login" replace />;
-//   }
-
-//   return <Outlet />;
-// };
-
-// export default ProtectedRoute;
-
-
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
