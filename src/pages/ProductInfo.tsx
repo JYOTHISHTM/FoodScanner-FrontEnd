44cd0d922d268d9059aa1c25e47c2e6a0e1e97fd
Jyothish T M <jothishtm2002@gmail.com>
2026-04-09 21:35:27 +0530

    refactor(pages): remove ProductInfo page

--- src/pages/ProductInfo.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const ProductInfo = () => {
-  const { id } = useParams();
-
-  return <h1>Product Info: {id}</h1>;
-};
-
-export default ProductInfo;
